663ac29075b67e54c0de90feb443c1736cfdcd8e
alogsDiu <sultanoverjan22@gmail.com>
2024-04-28 12:41:33 +0000

    cardreader say hello

--- attendance/views.py
+++ attendance/views.py
@@ -10,9 +10,9 @@ from account.models import Teacher, Student
 def att_toggle(request, att_id, course_code, group, course_type):
     att_row = attendance.objects.get(att_id=att_id)
     value=att_row.status
-    if value == 0 or value == 3: 
+    if value == 0 or value == 3:
         print("No")
-    else:    
+    else:
         value=(value+1)%3
         if value == 0 :
             value=1
@@ -26,13 +26,13 @@ def open_registration(request, course_code, group, course_type):
     is_lecture = False
     if course_type == "L":
         is_lecture = True
-    
+
     the_courses = specific_course.objects.filter(course_code=course_code, group=group, is_lecture=is_lecture)
-    
+
     registration_open_until = timezone.now() + timedelta(seconds=60)
 
     the_courses.update(registration_deadline=registration_open_until)
-    
+
     return redirect('dashboard:teacher_courses_specific_one', course_code=course_code, course_type=course_type)
 
 @login_required
@@ -41,12 +41,12 @@ def manual_attendance(request,course_code, is_lecture):
         is_lecture = True
     else:
         is_lecture = False
-    
+
     specific_course_ids_of_a_student = students_courses.objects.filter(student_id=request.user.student.id).values_list('specific_course_id', flat=True)
     needed_ids_for_attendance = specific_course.objects.filter(specific_course_id__in=specific_course_ids_of_a_student, is_lecture=is_lecture,course_code=course_code).values_list('specific_course_id', flat=True)
-    
+
     current_free_id = len(attendance.objects.all())
-    
+
     all_attendances = attendance.objects.filter(specific_course_id__in=needed_ids_for_attendance, student_id=request.user.student.id ).values_list('weak_count', flat=True)
     week_count=0
     if len(all_attendances)==0:
@@ -64,32 +64,29 @@ def manual_attendance(request,course_code, is_lecture):
             weak_count=week_count,  # Default weak_count, you can change it as needed
             att_id=current_free_id  # Default att_id, you can change it as needed
         )
-        
+
         attendance_item.save()
         current_free_id+=1
-    
+
     return redirect('dashboard:student_courses_specific_one', course_code=course_code)
 
 def card_reader(request):
     uid = request.GET.get('uid', '')
-    
+
     # Check if a UID is provided
     if uid:
         # Your logic here
-        
         id_of_a_student = card_of_student.objects.filter(uid=uid).first()
-        one_student = Student.objects.filter(student_id=id_of_a_student).first()
-        
-        
-        
+        one_student = Student.objects.filter(id=id_of_a_student.student_id).first()
+
         response_text = f"SALAMALEIKUM {one_student.first_name}"
-        
+
         response_status = 200  # HTTP status code 200 OK
     else:
         response_text = "No UID provided"
         response_status = 400  # HTTP status code 400 Bad Request
-    
-    
-    
+
+
+
     # Return an HTTP response with the response text
     return HttpResponse(response_text, content_type="text/plain", status=response_status)
